e769f2c925990a7c952b63ed14e756ed1533d8e8
Daniel <danman113@msn.com>
2021-04-02 11:10:53 -0700

    slightly different build server when on dokku

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 
 module.exports = (phase, { defaultConfig }) => ({
-  assetPrefix: phase === PHASE_DEVELOPMENT_SERVER ? '' : '/berezin.io/',
+  assetPrefix: phase === PHASE_DEVELOPMENT_SERVER && Boolean(process.env.DOKKU_ROOT) ? '' : '/berezin.io/',
 })
